66a3fdeec64fa8fab149b7009dcf8b65552dab21
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-04-21 14:04:17 +0300

    Thu Apr 21 14:04:17 IDT 2016

--- EG2DataMiningPackage/mac/ana_3psimulation.py
+++ EG2DataMiningPackage/mac/ana_3psimulation.py
@@ -9,7 +9,7 @@ ROOT.gStyle.SetOptStat(0000)
 
 DoAllVariables  = True
 
-Var         = "theta_vs_phi"
+Var         = "pp_scattering_CMtheta_vs_Mpp"
 Path        = "/Users/erezcohen/Desktop/DataMining/AnaFiles"
 analysis    = TAnalysis()
 Nbins       = 100
@@ -35,14 +35,16 @@ if DoAllVariables:
     elif (Var=="RecoilProtons"):
         ana.Draw2DVarAndCut(cSRC , 4 , "protons[1].P()"  ,  "protons[2].P()"   , Nbins , 0.2   , 1  , Nbins , 0.2   , 1
                                     , "recoil protons momenta" , "p(2) [GeV/c]" , "p(3) [GeV/c]" , ana.Sim3pSRCCut , True  , "3pSRC")
-
     elif (Var=="theta_vs_phi"):
         ana.Draw2DVarAndCut(cSRC , 5 , "fabs(phiMiss23)" , "thetaMiss23"   , Nbins , 0 , 80 , Nbins , 80 , 180
-                             ,"#phi vs. #theta - FSI simulation","|#phi| [deg.]","#theta [deg.]" , ana.Sim3pSRCCut )
+                        ,"#phi vs. #theta - FSI simulation","|#phi| [deg.]","#theta [deg.]" , ana.Sim3pSRCCut )
+    elif (Var=="pp_scattering_CMtheta_vs_Mpp"):
+        ana.H2("Mpp" , "(180/3.1415)*Theta_cm" , ROOT.TCut() ,"surf2" , Nbins , 1.9 , 4.2 , Nbins , 0 , 180
+           ,"#theta vs. #sqrt{s}","m(pp) [GeV/c^{2}]","#theta [deg.]" )
 
-                                            
 
-                        
+
+
     cSRC.Update()
     wait()
     cSRC.SaveAs(init.dirname()+"/FSI3p_sim_"+Var+".pdf")
