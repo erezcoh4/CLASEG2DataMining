855c21800b42f8951471fa7666317bef1aa5d42f
Erez Cohen <erezcohen@Erezs-iMac.local>
2016-06-29 21:38:41 +0300

    Wed Jun 29 21:38:41 IDT 2016

--- EG2DataMiningPackage/mac/ana_TwoSlowProtons.py
+++ EG2DataMiningPackage/mac/ana_TwoSlowProtons.py
@@ -1,5 +1,4 @@
-import ROOT
-import os, sys , math
+import ROOT ,os, sys , math
 from ROOT import TEG2dm , TPlots , TAnalysisEG2 , TSchemeDATA
 from rootpy.interactive import wait
 sys.path.insert(0, '/Users/erezcohen/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
@@ -10,73 +9,45 @@ import Initiation as init
 
 
 
-Operation   = "?pp vs rec. protons from ppp"
+Operation   = "compare different samples"
 Var         = sys.argv[1] if len(sys.argv)>1 else "Xb"
-p           = int(sys.argv[2]) if len(sys.argv)>2 else 0
 Nbins       = 40
 dm          = TEG2dm()
 Path        = "/Users/erezcohen/Desktop/DataMining/AnaFiles"
 
 
 
-print "Operation = " +Operation + ", Var = " + Var
+print "looking at sample of ?pp events, operation = " +Operation + ", Var = " + Var
 
-# ------------------------------------------------------------------ #
-if Operation == "?pp":
-    ana = TAnalysisEG2("C12_TwoSlowProtons")
-    cut = ROOT.TCut("Xb>1.05")
-    if (Var=="Pcm"):
-        plot_args = ["PcmFinalState.P()" , cut , Nbins , 0 , 1.5 , "|#vec{p} (c.m.)| [GeV/c]"]
-    elif (Var=="Xb"):
-        plot_args = [Var , cut , Nbins , 1 , 4 , "Bjorken x"]
-    elif (Var=="q"):
-        plot_args = ["q.P()" , "q.E()"  , cut , Nbins , 1.1 , 4 , Nbins , 0 , 3.1 ,"|#vec{q}| [GeV/c]" , "#omega [GeV]"]
-    elif (Var=="Q2"):
-        plot_args = [Var , cut , Nbins , 1 , 5 , "Q^{2} (GeV/c) ^{2}"]
-    elif (Var=="Wtilde"):
-        plot_args = ["Wtilde.Mag()" , cut , Nbins , -5 , 5 , "#sqrt{ W'^{#mu} W'_{#mu} } [GeV/c ^{2}]"]
-    elif (Var=="Wtilde_vs_Q2"):
-        plot_args = ["Wtilde.Mag()" ,"Q2" , cut , Nbins , -5 , 0 , Nbins , 1 , 5 ,"#sqrt{ W'^{#mu} W'_{#mu} } [GeV/c ^{2}]" , "Q^{2} (GeV/c) ^{2}"]
-    elif (Var=="p3momenta"):
-        plot_args = ["protons[0].P()" ,"protons[1].P()" , cut , Nbins , 0.27 , 0.73 , Nbins , 0.27 , 0.73 ,"|#vec{p}(1)| [GeV/c]" , "|#vec{p}(2)| [GeV/c]"]
-#        p = int(input("which proton ? [ 1 / 2 ]"))
-#        plot_args = ["protons[%d].P()"%(p-1) , cut , Nbins , 0.27 , 0.73 ,"|#vec{p}(%d)| [GeV/c]"%p]
-#    elif (Var=="cos_opening_angle"):
-#        plot_args = [ str(ana.CosTheta("protons[0].Vect()","protons[1].Vect()")) , cut , Nbins , -1 , 1 , "cos (#theta_{12})"]
-#    elif (Var=="opening_angle"):
-#        plot_args = [ str(ana.Theta("protons[0].Vect()","protons[1].Vect()")) , cut , Nbins , 0 , 180 , "#theta_{12} [deg.]"]
-#    elif (Var=="Mmiss"):
-#        plot_args = ["Pcm.Mag()", cut , Nbins , -3.5 , 2.1  , "#sqrt{|p^{#mu}_{1}+p^{#mu}_{2}-q^{#mu}|^{2}} [GeV/c ^{2}]"  ]
-##        plot_args = ["PcmFinalState.Mag()", cut , Nbins , -2 , 5  , "#sqrt{|p^{#mu}_{1}+p^{#mu}_{2}|^{2}} [GeV/c ^{2}]"  ]
-#    elif (Var=="Wmiss"):
-#        plot_args = ["Wmiss.Mag()" , cut , Nbins , -2.1 , 3 , "#sqrt{ |3M_{N} + q - p_{1} - p_{2}|^{2} } [GeV/c ^{2}]"]
-
-	print "ploting " + Var
-	gen_plot.plot(ana,"TwoSlowProtons_"+Var,plot_args)
 
 
 # ------------------------------------------------------------------ #
-
-
-# ------------------------------------------------------------------ #
-elif Operation == "?pp vs rec. protons from ppp":
+if Operation == "compare different samples":
  
-    Data = "no_physics"
+    XbCut = ROOT.TCut("Xb>1.05")
     
-    if (Data == "C12_Al27_Fe56_Pb28"):
-        ana = TAnalysisEG2("C12_Al27_Fe56_Pb28",ROOT.TCut("Xb>1.05"))
+    DataType = int(input("Data Type? (C12_TwoSlowProtons / FullCandidates / C12_TwoSlowProtons_ppp / no_physics)"))
+    Data  = sys.argv[2] if len(sys.argv)>2 else "C12_TwoSlowProtons"
+    if (Data == "FullCandidates"):
+        ana = TAnalysisEG2("C12_Al27_Fe56_Pb28",XbCut) # two recoilong protons + from our sample of candidates
         cut = ana.pppSRCCut
+    elif (Data == "C12_TwoSlowProtons"):
+        ana = TAnalysisEG2("C12_TwoSlowProtons",XbCut) # two slow protons + nothing else
+        cut = XbCut
     elif (Data == "C12_TwoSlowProtons_ppp"):
-        ana = TAnalysisEG2("C12_TwoSlowProtons_ppp",ROOT.TCut("Xb>1.05"))
-        cut = ROOT.TCut("Xb > 1.05")
+        ana = TAnalysisEG2("C12_TwoSlowProtons_ppp",XbCut) # two slow protons + additional fast proton (>1. GeV/c)
+        cut = XbCut
     elif (Data == "no_physics"):
-        ana = TAnalysisEG2("GSIM_run0091_eep",ROOT.TCut("Xb>1.05"))
+        ana = TAnalysisEG2("GSIM_run0091_eep",XbCut) # two slow protons from synthetic data of candidates + 2 random protons
         cut = ana.Sim3pSRCCut
 
 
 
     if (Var=="Pcm"):
-        plot_args = ["Prec.P()" , cut , Nbins , 0 , 1.5 , "|#vec{p} (c.m.)| [GeV/c]"]
+        if (Data == "C12_TwoSlowProtons"):
+            plot_args = ["PcmFinalState.P()" , cut , Nbins , 0 , 1.5 , "|#vec{p} (c.m.)| [GeV/c]"]
+        else:
+            plot_args = ["Prec.P()" , cut , Nbins , 0 , 1.5 , "|#vec{p} (c.m.)| [GeV/c]"]
     elif (Var=="Xb"):
         plot_args = [Var , cut , Nbins , 1 , 4 , "Bjorken x"]
     elif (Var=="q"):
@@ -85,8 +56,6 @@ elif Operation == "?pp vs rec. protons from ppp":
         plot_args = [Var , cut , Nbins , 1 , 5 , "Q^{2} (GeV/c) ^{2}"]
     elif (Var=="p3momenta"):
         plot_args = ["protons[1].P()" ,"protons[2].P()" , cut , Nbins , 0.27 , 0.73 , Nbins , 0.27 , 0.73 ,"|#vec{p}(1)| [GeV/c]" , "|#vec{p}(2)| [GeV/c]"]
-#        p = int(input("which proton ? [ 1 / 2 ]"))
-#        plot_args = ["protons[%d].P()"%p , cut , Nbins , 0.27 , 0.73 ,"|#vec{p}(%d)| [GeV/c]"%p]
     elif (Var=="cos_opening_angle"):
         plot_args = [ str(ana.CosTheta("protons[1].Vect()","protons[2].Vect()")) , cut , Nbins , -1 , 1 , "cos (#theta_{12})"]
     elif (Var=="opening_angle"):
@@ -96,7 +65,7 @@ elif Operation == "?pp vs rec. protons from ppp":
 
 
     print "ploting " + Var
-    gen_plot.plot(ana,"recProtons_"+Data+"_"+Var,plot_args)
+    gen_plot.plot(ana,"slowProtons_"+Data+"_"+Var,plot_args)
 
 
 # ------------------------------------------------------------------ #
