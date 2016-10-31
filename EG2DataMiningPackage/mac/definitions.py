7195bb6a74f3775e0e883802ea2a2facbbf4a594
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-10-31 17:19:45 +0200

    Mon Oct 31 17:19:45 IST 2016

---
+++ EG2DataMiningPackage/mac/definitions.py
@@ -0,0 +1,19 @@
+
+import ROOT,os, sys , math , os.path , math
+from ROOT import TEG2dm , TSchemeDATA
+sys.path.insert(0, '/Users/erezcohen/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
+sys.path.insert(0, '/home/erez/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
+import input_flags
+flags = input_flags.get_args()
+
+
+
+
+# paths
+if flags.worker == "erez":
+    path = "/Users/erezcohen/Desktop/DataMining"
+
+elif flags.worker == "helion":
+    path = "/home/erez/DataMining"
+
+
--- EG2DataMiningPackage/mac/scheme_file.py
+++ EG2DataMiningPackage/mac/scheme_file.py
@@ -4,20 +4,20 @@
     python mac/scheme_file.py -A12  --option="(e,e'pp?)" --DataType=New_NoCTofDATA
     python mac/scheme_file.py -A12  --option=GSIM -r93
 '''
+#
+#import ROOT,os, sys , math , os.path , math
+#from ROOT import TEG2dm , TSchemeDATA
+#sys.path.insert(0, '/Users/erezcohen/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
+#sys.path.insert(0, '/home/erez/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
+#import input_flags
+#flags = input_flags.get_args()
+from definitions import *
 
-import ROOT,os, sys , math , os.path , math
-from ROOT import TEG2dm , TSchemeDATA
-sys.path.insert(0, '/Users/erezcohen/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
-sys.path.insert(0, '/home/erez/larlite/UserDev/mySoftware/MySoftwarePackage/mac')
-import input_flags
-flags = input_flags.get_args()
-
-
-if flags.worker == "erez":
-    path = "/Users/erezcohen/Desktop/DataMining"
-
-elif flags.worker == "helion":
-    path = "/home/erez/DataMining"
+#if flags.worker == "erez":
+#    path = "/Users/erezcohen/Desktop/DataMining"
+#
+#elif flags.worker == "helion":
+#    path = "/home/erez/DataMining"
 
 
 dm          = TEG2dm()
