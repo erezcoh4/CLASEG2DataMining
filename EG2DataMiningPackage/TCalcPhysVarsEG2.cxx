5c698f831385865d8c559a3ccd65ca7b6f379ca0
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-10-31 18:14:04 +0200

    Mon Oct 31 18:14:04 IST 2016

--- EG2DataMiningPackage/TCalcPhysVarsEG2.cxx
+++ EG2DataMiningPackage/TCalcPhysVarsEG2.cxx
@@ -154,14 +154,14 @@ void TCalcPhysVarsEG2::InitOutputTree(){
     OutTree -> Branch("PcmFinalState"       ,"TLorentzVector"       ,&PcmFinalState);
     OutTree -> Branch("Plead"               ,"TLorentzVector"       ,&Plead);
     OutTree -> Branch("Prec"                ,"TLorentzVector"       ,&Prec);
-    OutTree -> Branch("q"                   ,"TLorentzVector"       ,&q);
-    OutTree -> Branch("e"                   ,"TLorentzVector"       ,&e);
-    OutTree -> Branch("Wtilde"              ,"TLorentzVector"       ,&Wtilde);
-    OutTree -> Branch("Wmiss"               ,"TLorentzVector"       ,&Wmiss);
-    OutTree -> Branch("WmissWithCm"         ,"TLorentzVector"       ,&WmissWithCm);
-    OutTree -> Branch("WmissCmEps"          ,"TLorentzVector"       ,&WmissCmEps);
-    OutTree -> Branch("protons"             ,&protons);             // std::vector<TLorentzVector>
-    OutTree -> Branch("protonsLab"          ,&protonsLab);          // std::vector<TLorentzVector>
+//    OutTree -> Branch("q"                   ,"TLorentzVector"       ,&q);
+//    OutTree -> Branch("e"                   ,"TLorentzVector"       ,&e);
+//    OutTree -> Branch("Wtilde"              ,"TLorentzVector"       ,&Wtilde);
+//    OutTree -> Branch("Wmiss"               ,"TLorentzVector"       ,&Wmiss);
+//    OutTree -> Branch("WmissWithCm"         ,"TLorentzVector"       ,&WmissWithCm);
+//    OutTree -> Branch("WmissCmEps"          ,"TLorentzVector"       ,&WmissCmEps);
+//    OutTree -> Branch("protons"             ,&protons);             // std::vector<TLorentzVector>
+//    OutTree -> Branch("protonsLab"          ,&protonsLab);          // std::vector<TLorentzVector>
 
     
     
