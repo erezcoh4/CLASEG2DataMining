02ef50ee671ad2ec4d0dc794f505858cf2994f5e
Erez Cohen <cohen.erez7@gmail.com>
2016-11-29 10:22:55 +0200

    Tue Nov 29 10:22:55 IST 2016

--- EG2DataMiningPackage/TCalcPhysVarsEG2.cxx
+++ EG2DataMiningPackage/TCalcPhysVarsEG2.cxx
@@ -440,7 +440,7 @@ void TCalcPhysVarsEG2::ComputePhysVars(int entry){
     OutTree -> Fill();
     Printf("OutTree -> Fill();");
 
-    TLorentzVector in1(vec1.size(), vec1.size(), vec1.size(), vec1.size());
+    TLorentzVector in1(protons[0].Px(),protons[0].Py(),protons[0].Pz(),protons[0].E());
     vec1.push_back(in1);
     tree->Fill();
     Printf("tree -> Fill();");
