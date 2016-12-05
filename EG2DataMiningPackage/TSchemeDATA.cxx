39bec8a3d9c3b52c2b6ef135370f5ab91dc38f90
Erez Cohen <cohen.erez7@gmail.com>
2016-12-04 20:58:49 -0500

    Sun Dec  4 20:58:49 EST 2016

--- EG2DataMiningPackage/TSchemeDATA.cxx
+++ EG2DataMiningPackage/TSchemeDATA.cxx
@@ -365,7 +365,7 @@ void TSchemeDATA::SchemeOnTCut(TString Path, TString fInFileName, TString fInTre
     Printf("scheming from %s/%s",Path.Data() , fInFileName.Data());
     TFile * TmpInFile = new TFile(Form("%s/%s",Path.Data() , fInFileName.Data()));
     TTree * TmpTree = (TTree*) TmpInFile -> Get(fInTreeName);
-    TmpInFile->Close();
+//    TmpInFile->Close();
     Printf("scheming to %s/%s",Path.Data(), fOutFileName.Data());
 //    TFile * TmpOutFile = new TFile(Form("%s/%s",Path.Data(), fOutFileName.Data()),"recreate");
     Printf("opened output file, writing %lld entries",TmpTree -> GetEntries());
