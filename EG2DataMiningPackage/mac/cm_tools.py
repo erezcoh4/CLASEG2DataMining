d93f784d42817dfbe59882e904613e6ce6a02952
Erez Cohen <cohen.erez7@gmail.com>
2016-12-20 19:04:59 +0200

    Tue Dec 20 19:04:59 IST 2016

--- EG2DataMiningPackage/mac/cm_tools.py
+++ EG2DataMiningPackage/mac/cm_tools.py
@@ -466,7 +466,8 @@ def generate_runs_with_different_parameters( option,
     pAcceptacneFile = ROOT.TFile(path+"/GSIM_DATA/PrecoilAcceptance.root")
     print 'path+/GSIM_DATA/PrecoilAcceptance.root:',path+"/GSIM_DATA/PrecoilAcceptance.root"
     path = path + "/Analysis_DATA/ppSRCcm"
-
+    if 'helion' in worker:
+        path = "/extra/Erez/DataMining"
 
     # simulation pandas::DataFrame
     df_reco_parameters = pd.DataFrame(columns=cm_pars_columns)
